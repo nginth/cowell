eb7b79fb22076d8ff7d0fa4f52eb67f0311665c4
Nick Ginther <nginth@gmail.com>
2017-07-01 14:36:20 -0500

    add start script

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 var express = require('express');
 
+const app = express()
+const PORT = 8080
+
+app.get('/', (req, res) => {
+    res.send('hello express')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
