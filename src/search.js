007f2edc81273254b2dba5900773cc2a599f7a03
Nick Ginther <nginth@gmail.com>
2017-07-01 20:53:17 -0500

    add apple music (itunes) support

--- src/search.js
+++ src/search.js
@@ -10,6 +10,66 @@ function handleSearch(query, access_token) {
 }
 
 function albumSearch(query, access_token) {
+    return Promise.all([spotifyAlbumSearch(query, access_token), appleAlbumSearch(query)])
+        .then((responses) => {
+            return {
+                template: 'albums',
+                templateArgs: {
+                    spotify: {
+                        albums: responses[0].albums.items,
+                        albumName: query.album
+                    },
+                    apple: {
+                        albums: responses[1].results
+                    }
+                }
+            }
+        });
+}
+
+function albumArtistSearch(query, access_token) {
+    return Promise.all([spotifyAlbumArtistSearch(query, access_token), appleAlbumArtistSearch(query)])
+        .then((responses) => {
+            console.log(JSON.stringify(responses[1]));
+            return {
+                template: 'albums',
+                templateArgs: {
+                    albumName: query.album,
+                    spotify: {
+                        albums: responses[0].albums.items
+                    },
+                    apple: {
+                        albums: responses[1].results
+                    }
+                }
+            }
+        });
+}
+
+function appleAlbumSearch(query) {
+    const encodedAlbum = encodeQs(query.album);
+    const options = {
+        uri: `https://itunes.apple.com/search?term=${encodedAlbum}&entity=album&media=music`,
+    }
+    return request.get(options)
+        .then((appleRes) => {
+            return JSON.parse(appleRes);
+        });
+}
+
+function appleAlbumArtistSearch(query) {
+    const encodedAlbum = encodeQs(query.album);
+    const encodedArtist = encodeQs(query.artist);
+    const options = {
+        uri: `https://itunes.apple.com/search?term=${encodedAlbum}+${encodedArtist}&entity=album&media=music`,
+    }
+    return request.get(options)
+        .then((appleRes) => {
+            return JSON.parse(appleRes);
+        });
+}
+
+function spotifyAlbumSearch(query, access_token) {
     const options = {
         uri: 'https://api.spotify.com/v1/search',
         qs: {
@@ -21,19 +81,10 @@ function albumSearch(query, access_token) {
         },
         json: true
     }
-    return request.get(options)
-        .then((searchRes) => {
-            return {
-                template: 'albums',
-                templateArgs: {
-                    albums: searchRes.albums.items,
-                    albumName: query.album
-                }
-            }
-        });
+    return request.get(options);
 }
 
-function albumArtistSearch(query, access_token) {
+function spotifyAlbumArtistSearch(query, access_token) {
     const encodedAlbum = encodeQs(query.album);
     const encodedArtist = encodeQs(query.artist);
     const qs = `q=album:${encodedAlbum}%20artist:${encodedArtist}&type=album`;
@@ -46,21 +97,11 @@ function albumArtistSearch(query, access_token) {
         json: true
     }
     console.log(access_token);
-    return request.get(options)
-        .then((searchRes) => {
-            return {
-                template: 'albums',
-                templateArgs: {
-                    albums: searchRes.albums.items,
-                    albumName: query.album,
-                    artistName: query.artist
-                }
-            }
-        });
+    return request.get(options);
 }
 
 function encodeQs(str) {
-    return str.replace(' ', '%20');
+    return str.replace(' ', '+');
 }
 
 function isEmpty(str) {
