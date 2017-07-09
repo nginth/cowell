925823751a8378a07c59ab3aafd5649f83b5a6be
Nick Ginther <nginth@gmail.com>
2017-07-08 22:29:59 -0500

    add caching and recent searches

--- index.js
+++ index.js
@@ -1,47 +1,89 @@
-'use strict';
-var express = require('express');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var auth = require('./src/auth.js');
-var search = require('./src/search.js');
-var error = require('./src/error.js');
-var constants = require('./src/constants.js');
-var request = require('request-promise');
+'use strict'
+var express = require('express')
+var bodyParser = require('body-parser')
+var expressValidator = require('express-validator')
+var auth = require('./src/auth.js')
+var search = require('./src/search.js')
+var error = require('./src/error.js')
+var constants = require('./src/constants.js')
+var redis = require('redis')
+var bluebird = require('bluebird')
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
 
-const PORT = (process.env.PORT || 8080);
+const PORT = (process.env.PORT || 8080)
+const REDIS_PORT = (process.env.REDIS_URL || 6379)
+const RECENT_LIST = 'recents'
+const RECENT_SIZE = 10
 
-const app = express();
+const app = express()
+const redisClient = redis.createClient(REDIS_PORT)
 
-app.use(express.static('assets'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressValidator())
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Cowell', BASE_URL: constants.BASE_URL});
-});
+  redisClient.lrangeAsync(RECENT_LIST, 0, RECENT_SIZE - 1)
+    .then((keys) => {
+      const templateArgs = {title: 'Cowell', BASE_URL: constants.BASE_URL}
+      if (keys != null) {
+        templateArgs.recentSearches = []
+        keys.forEach((key) => {
+          const keySplit = key.split('^')
+          templateArgs.recentSearches.push({
+            album: keySplit[0],
+            artist: keySplit[1]
+          })
+        })
+      }
+      res.render('index', templateArgs)
+    })
+})
 
-app.get('/search', (req, res) => {    
-    req.sanitize('album').escape();
-    req.sanitize('album').trim();
-    req.sanitize('artist').escape();
-    req.sanitize('artist').trim();
-
-    auth.getSpotifyToken(process.env.SPOTIFY_CLIENT, process.env.SPOTIFY_SECRET, process.env.SPOTIFY_REFRESH)
+app.get('/search', cache, (req, res) => {
+  auth.getSpotifyToken(process.env.SPOTIFY_CLIENT, process.env.SPOTIFY_SECRET, process.env.SPOTIFY_REFRESH)
         .then(authRes => search.handleSearch(req.query, authRes.access_token))
         .then((searchRes) => {
-            searchRes.templateArgs.BASE_URL = constants.BASE_URL;
-            res.render(searchRes.template, searchRes.templateArgs)
+          searchRes.templateArgs.BASE_URL = constants.BASE_URL
+          const key = `${req.query.album}^${req.query.artist}`
+          redisClient.setex(key, 3600, JSON.stringify(searchRes))
+          redisClient.lpush(RECENT_LIST, key)
+          redisClient.ltrim(RECENT_LIST, 0, RECENT_SIZE - 1)
+          res.render(searchRes.template, searchRes.templateArgs)
         })
-        .catch(err => error.handleError(err, res));
-});
+        .catch(err => error.handleError(err, res))
+})
 
 app.get('/about', (req, res) => {
-    res.render('about', {BASE_URL: constants.BASE_URL});
-});
- 
+  res.render('about', {BASE_URL: constants.BASE_URL})
+})
+
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
-});
+  console.log(`listening on ${PORT}`)
+})
+
+function cache (req, res, next) {
+  req.sanitize('album').escape()
+  req.sanitize('album').trim()
+  req.sanitize('artist').escape()
+  req.sanitize('artist').trim()
+
+  const key = `${req.query.album}^${req.query.artist}`
+  console.log(key)
+  redisClient.getAsync(key)
+    .then(data => {
+      if (data != null) {
+        const searchRes = JSON.parse(data)
+        searchRes.templateArgs.fetchUrl = search.fetchUrl
+        searchRes.templateArgs.fetchFromObject = search.fetchFromObject
+        res.render(searchRes.template, searchRes.templateArgs)
+      } else {
+        next()
+      }
+    })
+    .catch(err => console.log(err))
+}
--- src/search.js
+++ src/search.js
@@ -1,93 +1,94 @@
-'use strict';
-var request = require('request-promise');
-var spotifyAPI = require('./api/spotifyapi.js');
-var appleAPI = require('./api/appleapi.js');
-var tidalAPI = require('./api/tidalapi.js');
+'use strict'
+var spotifyAPI = require('./api/spotifyapi.js')
+var appleAPI = require('./api/appleapi.js')
+var tidalAPI = require('./api/tidalapi.js')
 
-function handleSearch(query, access_token) {
-    const params = {
-        album: query.album,
-        artist: query.artist
-    }
-    return Promise.all([spotifyAPI.search(params, access_token), appleAPI.search(params), tidalAPI.search(params)])
+function handleSearch (query, accessToken) {
+  const params = {
+    album: query.album,
+    artist: query.artist
+  }
+  return Promise.all([spotifyAPI.search(params, accessToken), appleAPI.search(params), tidalAPI.search(params)])
         .then((responses) => {
-            console.log(responses[2].albums.items[0]);
-            return {
-                template: 'albums',
-                templateArgs: {
-                    queryAlbumName: (query.album || query.artist),
-                    fetchFromObject: fetchFromObject,
-                    fetchUrl: fetchUrl,
-                    apis: [
-                        {
-                            apiName: 'Spotify',
-                            albums: responses[0].albums.items,
-                            albumName: 'name',
-                            albumUrl: 'external_urls.spotify',
-                            imageUrl: 'images.2.url',
-                            artistUrl: 'external_urls.spotify',
-                            artists: 'artists',
-                            artistName: 'name',
-                            imageWidth: '60px',
-                            imageHeight: '60px'
-                        },
-                        {   
-                            apiName: 'Apple Music',
-                            albums: responses[1].results,
-                            albumName: 'collectionName',
-                            albumUrl: 'collectionViewUrl',
-                            imageUrl: 'artworkUrl60',
-                            artistUrl: 'artistViewUrl',
-                            artistName: 'artistName',
-                            imageWidth: '60px',
-                            imageHeight: '60px'
-                        },
-                        {
-                            apiName: 'Tidal',
-                            albums: responses[2].albums.items,
-                            albumName: 'title',
-                            albumUrl: 'url',
-                            imageUrl: 'cover',
-                            imageUrlPrefix: 'https://resources.wimpmusic.com/images/',
-                            imageUrlSuffix: '/80x80.jpg',
-                            artists: 'artists',
-                            artistUrl: 'id',
-                            artistUrlPrefix: 'http://www.tidal.com/artist/',
-                            artistName: 'name',
-                            imageWidth: '60px',
-                            imageHeight: '60px'
-                        }
-                    ]
+          console.log(responses[2].albums.items[0])
+          return {
+            template: 'albums',
+            templateArgs: {
+              queryAlbumName: (query.album || query.artist),
+              fetchFromObject: fetchFromObject,
+              fetchUrl: fetchUrl,
+              apis: [
+                {
+                  apiName: 'Spotify',
+                  albums: responses[0].albums.items,
+                  albumName: 'name',
+                  albumUrl: 'external_urls.spotify',
+                  imageUrl: 'images.2.url',
+                  artistUrl: 'external_urls.spotify',
+                  artists: 'artists',
+                  artistName: 'name',
+                  imageWidth: '60px',
+                  imageHeight: '60px'
+                },
+                {
+                  apiName: 'Apple Music',
+                  albums: responses[1].results,
+                  albumName: 'collectionName',
+                  albumUrl: 'collectionViewUrl',
+                  imageUrl: 'artworkUrl60',
+                  artistUrl: 'artistViewUrl',
+                  artistName: 'artistName',
+                  imageWidth: '60px',
+                  imageHeight: '60px'
+                },
+                {
+                  apiName: 'Tidal',
+                  albums: responses[2].albums.items,
+                  albumName: 'title',
+                  albumUrl: 'url',
+                  imageUrl: 'cover',
+                  imageUrlPrefix: 'https://resources.wimpmusic.com/images/',
+                  imageUrlSuffix: '/80x80.jpg',
+                  artists: 'artists',
+                  artistUrl: 'id',
+                  artistUrlPrefix: 'http://www.tidal.com/artist/',
+                  artistName: 'name',
+                  imageWidth: '60px',
+                  imageHeight: '60px'
                 }
+              ]
             }
-        });
+          }
+        })
 }
 
-function fetchUrl(obj, prop, prefix, suffix) {
-    prefix = (prefix || '');
-    suffix = (suffix || '');
-    let url = fetchFromObject(obj, prop);
-    if (prefix.indexOf('wimpmusic') !== -1) url = url.replace(/-/g, '/');
-    return prefix + url + suffix;
+function fetchUrl (obj, prop, prefix, suffix) {
+  prefix = (prefix || '')
+  suffix = (suffix || '')
+  let url = fetchFromObject(obj, prop)
+  if (prefix.indexOf('wimpmusic') !== -1) url = url.replace(/-/g, '/')
+  return prefix + url + suffix
 }
 
-function fetchFromObject(obj, prop) {
-  //property not found
-  if (typeof obj === 'undefined') return false;
+function fetchFromObject (obj, prop) {
+  // property not found
+  if (typeof obj === 'undefined') return false
 
-  //index of next property split
+  // index of next property split
   var _index = prop.indexOf('.')
 
-  //property split found; recursive call
+  // property split found; recursive call
   if (_index > -1) {
-    //get object at property (before split), pass on remainder
-    return fetchFromObject(obj[prop.substring(0, _index)], prop.substr(_index + 1));
+    // get object at property (before split), pass on remainder
+    return fetchFromObject(obj[prop.substring(0, _index)], prop.substr(_index + 1))
   }
 
-  //no split; get property
-  return obj[prop];
+  // no split; get property
+  return obj[prop]
 }
 
 module.exports = {
-    handleSearch: handleSearch
+  handleSearch: handleSearch,
+  fetchUrl: fetchUrl,
+  fetchFromObject: fetchFromObject
 }
